54bc4801352c82eef2039faafa1e3c59c39f2cd2
Lucas Araya <luccasniccolas2002@gmail.com>
2025-10-04 22:25:20 -0300

    backend activo

--- revitek/server/apps/estados/views.py
+++ revitek/server/apps/estados/views.py
@@ -1,8 +1,3 @@
-from rest_framework.generics import RetrieveAPIView
-from apps.agenda.models import Reserva
-from .serializers import EstadoDetalleSerializer
+from django.shortcuts import render
 
-class EstadoReservaView(RetrieveAPIView):
-    queryset = Reserva.objects.all().prefetch_related("historial")
-    serializer_class = EstadoDetalleSerializer
-    lookup_field = "pk"
+# Create your views here.
