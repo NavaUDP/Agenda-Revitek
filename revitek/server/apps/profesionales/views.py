810ebce4a84061f21dedbf16cc61162fbefd9db0
Diego Navarrete <diego.navarrete2@mail.udp.cl>
2025-10-20 20:17:01 -0300

    profesionales en main

--- revitek/server/apps/profesionales/views.py
+++ revitek/server/apps/profesionales/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# revitek/server/apps/profesionales/views.py
+from rest_framework import viewsets
+from .models import Profesional
+from .serializers import ProfesionalSerializer
+# Podrías añadir permisos si es necesario, ej: from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+class ProfesionalViewSet(viewsets.ModelViewSet):
+    queryset = Profesional.objects.filter(activo=True) # Mostrar solo activos por defecto
+    serializer_class = ProfesionalSerializer
+    # permission_classes = [IsAdminUser] # Descomenta para restringir a admins
