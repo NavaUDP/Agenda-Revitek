b963a46431c9308b43d53106458e200eed9ec6ca
Lucas Araya <luccasniccolas2002@gmail.com>
2025-10-21 14:40:14 -0300

    Refactor: Elimina carpeta 'backend' antigua

--- revitek/backend/apps/profesionales/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_profesionales, name='get-profesionales'),
-    path('<int:id>', views.get_profesional_by_id, name='get-profesional-by-id')
-]
